0a0d726f811a20341463ecb3af41946f1879fb97
Maxim <maxim.bakal4ul@outlook.com>
2024-12-09 15:39:52 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace C_HW1
+{
+    class Program
+    {
+        static void PrintLadder(int steps)
+        {
+            int indent = 0;
+
+            for (int i = 0; i < steps; i++)
+            {
+                Console.WriteLine(new string(' ', indent) + "***");
+
+                indent += 2;
+                Console.WriteLine(new string(' ', indent) + "*");
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество ступенек: ");
+
+            if (int.TryParse(Console.ReadLine(), out int steps) && steps > 0)
+            {
+                PrintLadder(steps);
+            }
+            else
+            {
+                Console.WriteLine("Введите положительное число!");
+            }
+        }
+    }
+
+}
